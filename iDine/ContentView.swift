47646ee0195849de494f5250171856ad8efb1941
Ashish Kenjale <ashishkenjale@gmail.com>
2019-09-18 16:29:24 -0700

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Ashish Kenjale on 9/18/19.
+//  Copyright © 2019 Ashish Kenjale. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
